de8862b1cbfaaa80e414b74a3211b231fcfb1376
Debasish Padhan <53099388+debasishpadhan@users.noreply.github.com>
2022-02-18 11:52:06 +0530

    Update aws-key-pair.tf

--- aws-key-pair.tf
+++ aws-key-pair.tf
@@ -2,5 +2,5 @@
 
 resource "aws_key_pair" "key-tf" {
   key_name   = "deployer-key"
-  public_key = file("${path.module}/id_rsa.pub")
+  public_key = file("/var/lib/jenkins/id_rsa_key/id_rsa.pub")
 }
